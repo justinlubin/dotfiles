eb7eacd55bb4d9e08405538424b85acb7cd005c3
Justin Lubin <jlubi333@gmail.com>
2014-10-11 21:02:55 -0400

    Updated tmux config.

---
+++ zelda-battery.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Source: https://gist.github.com/stephencelis/4324139
+
+battery="$(pmset -g ps | awk 'NR==2' | perl -pe 's/.*?(\d+)%.*/\1/')"
+
+if [[ $battery -lt 10 ]]; then
+    echo -n "#[blink]♥ #[noblink]♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ "
+elif [[ $battery -lt 20 ]]; then
+    echo -n "♥ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ "
+elif [[ $battery -lt 30 ]]; then
+    echo -n "♥ ♥ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ "
+elif [[ $battery -lt 40 ]]; then
+    echo -n "♥ ♥ ♥ ♡ ♡ ♡ ♡ ♡ ♡ ♡ "
+elif [[ $battery -lt 50 ]]; then
+    echo -n "♥ ♥ ♥ ♥ ♡ ♡ ♡ ♡ ♡ ♡ "
+elif [[ $battery -lt 60 ]]; then
+    echo -n "♥ ♥ ♥ ♥ ♥ ♡ ♡ ♡ ♡ ♡ "
+elif [[ $battery -lt 70 ]]; then
+    echo -n "♥ ♥ ♥ ♥ ♥ ♥ ♡ ♡ ♡ ♡ "
+elif [[ $battery -lt 80 ]]; then
+    echo -n "♥ ♥ ♥ ♥ ♥ ♥ ♥ ♡ ♡ ♡ "
+elif [[ $battery -lt 90 ]]; then
+    echo -n "♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♡ ♡ "
+elif [[ $battery -lt 100 ]]; then
+    echo -n "♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♡ "
+else
+    echo -n "♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♡ "
+fi
+
+echo " ${battery}%" 
