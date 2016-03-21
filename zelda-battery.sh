e0c6a1959feb9fa17741d78f255bc86822688fc1
Justin Lubin <jlubi333@gmail.com>
2016-03-20 22:03:01 -0400

    Vastly improve zelda battery script.

--- zelda-battery.sh
+++ zelda-battery.sh
@@ -5,42 +5,48 @@
 # Test if we have the pmset command
 command -v pmset >/dev/null 2>&1 || exit
 
+# Get system information
 battery="$(pmset -g ps | awk 'NR==2' | perl -pe 's/.*?(\d+)%.*/\1/')"
 discharging="$(pmset -g ps | grep dis)"
 
-color="09"
+# Configuration
+oncolor="09"
+offcolor="19"
+onheart="♡ "
+offheart="♡ "
+chargingcolor="11"
+
+# Set color depending on charging status
 if [[ -z $discharging ]]; then
-    color="11"
+    oncolor=$chargingcolor
 fi
 
-echo -n "#[fg=colour$color]"
+# Set up variables
+heartcount=$((battery / 10))
+i=0
 
-if [[ $battery -lt 10 ]]; then
-    echo -n "#[blink]♥ #[noblink]♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ "
-elif [[ $battery -lt 20 ]]; then
-    echo -n "♥ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ "
-elif [[ $battery -lt 30 ]]; then
-    echo -n "♥ ♥ ♡ ♡ ♡ ♡ ♡ ♡ ♡ ♡ "
-elif [[ $battery -lt 40 ]]; then
-    echo -n "♥ ♥ ♥ ♡ ♡ ♡ ♡ ♡ ♡ ♡ "
-elif [[ $battery -lt 50 ]]; then
-    echo -n "♥ ♥ ♥ ♥ ♡ ♡ ♡ ♡ ♡ ♡ "
-elif [[ $battery -lt 60 ]]; then
-    echo -n "♥ ♥ ♥ ♥ ♥ ♡ ♡ ♡ ♡ ♡ "
-elif [[ $battery -lt 70 ]]; then
-    echo -n "♥ ♥ ♥ ♥ ♥ ♥ ♡ ♡ ♡ ♡ "
-elif [[ $battery -lt 80 ]]; then
-    echo -n "♥ ♥ ♥ ♥ ♥ ♥ ♥ ♡ ♡ ♡ "
-elif [[ $battery -lt 90 ]]; then
-    echo -n "♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♡ ♡ "
-elif [[ $battery -lt 100 ]]; then
-    echo -n "♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♡ "
-else
-    echo -n "♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ ♥ "
+# On hearts
+echo -n "#[fg=colour$oncolor]"
+if [[ $heartcount -eq 0 ]]; then
+    echo -n "#[blink]${onheart}#[noblink]"
+    i=1
 fi
+while [ $i -lt $heartcount ]; do
+    echo -n "${onheart}"
+    i=$((i + 1))
+done
 
+# Off hearts
+echo -n "#[fg=colour$offcolor]"
+while [ $i -lt 10 ]; do
+    echo -n "${offheart}"
+    i=$((i + 1))
+done
+
+# Output lightning if charging
 if [[ -z $discharging ]]; then
     echo -n " ⚡  "
 fi
 
+# Output the battery percentage
 echo " ${battery}%"
