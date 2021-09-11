7ae9be51c94e82e5c5c148da77fdb37995096571
Justin Lubin <jlubi333@gmail.com>
2021-09-10 20:00:42 -0700

    Update dotfiles

--- config.fish
+++ config.fish
@@ -78,6 +78,32 @@ function fish_prompt
     (set_color normal)
 end
 
+# !! and !$
+
+function bind_bang
+  switch (commandline -t)[-1]
+    case "!"
+      commandline -t $history[1]; commandline -f repaint
+    case "*"
+      commandline -i !
+  end
+end
+
+function bind_dollar
+  switch (commandline -t)[-1]
+    case "!"
+      commandline -t ""
+      commandline -f history-token-search-backward
+    case "*"
+      commandline -i '$'
+  end
+end
+
+function fish_user_key_bindings
+  bind ! bind_bang
+  bind '$' bind_dollar
+end
+
 # Aliases
 
 alias ls "ls -G"
